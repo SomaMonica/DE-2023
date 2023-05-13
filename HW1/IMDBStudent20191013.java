f1a7363198ee0268546e5791bf4c1193389fc13f
suMin <91327587+SomaMonica@users.noreply.github.com>
2023-05-13 09:29:08 +0900

    Update IMDBStudent20191013.java
    Import hadoop config

--- HW1/IMDBStudent20191013.java
+++ HW1/IMDBStudent20191013.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.conf.Configuration;
 
 public class Movie {
 	public static MovieMapper extends Mapper<Object, Text, Text, IntWritable>{
