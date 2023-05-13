0373ed94d2ccd2832a0e75d39d650110894dd722
suMin <91327587+SomaMonica@users.noreply.github.com>
2023-05-13 10:14:49 +0900

    Update UBERStudent20191013.java

--- HW1/UBERStudent20191013.java
+++ HW1/UBERStudent20191013.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.conf.Configuration;
 
 public class Uber {
 	public static String getDay(String d) throws IOException, InterruptedException {
