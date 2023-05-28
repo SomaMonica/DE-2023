d0be7ede55f2d2f56c6728d75b5991f20b0ff171
suMin <91327587+SomaMonica@users.noreply.github.com>
2023-05-28 18:39:58 +0900

    Update IMDBStudent20191013.java

--- HW2/IMDBStudent20191013.java
+++ HW2/IMDBStudent20191013.java
@@ -41,6 +41,9 @@ public class IMDBStudent20191013 {
 			if(ret != 0) return ret;
 			return tableName.compareTo(d.tableName); //M --> R
 		}
+		public String toString(){
+			return movieId + \t + tableName;
+		}
 	}
 	public static class FirstPartitioner extends Partitioner<DoubleKey, Text>{
 		public int getPartition(DoubleKey key, Text value, int numPartition) {
@@ -73,7 +76,7 @@ public class IMDBStudent20191013 {
 			return rslt;
 		}
 	}
-	public static class JoinNAvgMapper extends Mapper<LongWritable, Text, DoubleKey, Text>{
+	public static class TopKMovieMapper extends Mapper<Object, Text, DoubleKey, Text>{
 		boolean isMovie = true;
 		
 		protected void setup(Context ctx) throws IOException, InterruptedException{
@@ -81,50 +84,61 @@ public class IMDBStudent20191013 {
 			if(filename.indexOf("movies.dat") == -1) isMovie = false;
 		}
 		public boolean isFantasy(String genres) {
-			if(genres.toLowerCase().contains("fantasy")) return true;
+			if(genres.contains("Fantasy")) return true;
 			else return false;
 		}
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
 			String val[] = value.toSting().split("::"); 
  			if(isMovie) {
 				if(isFantasy(val[2])) {
 					DoubleKey outputK = new DoubleKey(val[0], "M");
-					context.write(outputK, new Text(val[1])); // Assasins (1995)
+					context.write(outputK, new Text("Movie:" + val[1])); // Movie:Assasins (1995)
 				}
 			}else {
 				DoubleKey outputK = new DoubleKey(val[1], "R");
-				context.write(outputK, new Text(val[2])); // 5
+				context.write(outputK, new Text("Rating:" + val[2])); // Rating:5
 			}
 		}
 	}
-	public static class JoinNAvgReducer extends Reducer<DoubleKey, Text, Text, DoubleWritable>{
-		public static boolean isNumeric(String str) { 
-			  try {  
-			    Double.parseDouble(str);  
-			    return true;
-			  } catch(NumberFormatException e){  
-			    return false;  
-			  }  
-			}
+	public static class TopKMovieReducer extends Reducer<DoubleKey, Text, Text, DoubleWritable>{
+		private PriorityQueue<Movie> queue;
+		private Comparator<Movie> comp = new MovieComparator();
+		private int topK;
+		
 		public void reduce(DoubleKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 			int cnt = 0;
 			double sum = 0;
 			String movieTitle = "";
-			for(Text val : values) {
-				if(cnt == 0) { //최초 1회
-					movieTitle = val.toString().trim();
-					if(isNumeric(movieTitle)) { // this isn't Fantasy movie
-						break; // out of for-each loop
+			
+			for(Text val : values){
+				String[] v = val.toString().split(":");
+				String fileName = v[0];
+				if(cnt == 0){ //최초 1회
+					if(fileName.equals("Rating")){
+						break;
 					}
-					cnt++;
-				}else {
-					sum += Double.parseDouble(val.toString().trim());
-					cnt++;
+					movieTitle = v[1];
+				}else{
+					sum += Double.parseDouble(v[1]);
 				}
+				cnt++;
 			}
-			cnt -= 1;
-			if(sum > 0) {
-				context.write(new Text(movieTitle), new DoubleWritable(sum/cnt));
+			if(sum > 0){
+				double avg = sum/(cnt-1);
+				insertQueue(queue, movieTitle, avg, topK);
+			}
+		}
+		
+		protected void setup(Context context) throws IOException, InterruptedException{
+			Configuration conf = context.getConfiguration();
+			topK = conf.getInt("topK" , -1);
+			queue = new PriorityQueue<Movie> (topK, comp); //comp 기준으로 size가 topK인 queue 생성
+		}
+		
+		protected void cleanup(Context context) throws IOException, InterruptedException{
+			while(queue.size() != 0) { //queue가 0될 때까지
+				Movie movie = (Movie) queue.remove();
+				context.write(new Text(movie.getTitle()), new DoubleWritable(movie.getAvgRating())); 
 			}
 		}
 	}
@@ -167,100 +181,36 @@ public class IMDBStudent20191013 {
 		}
 	}
 	
-	public static class TopKMapper extends Mapper<Object, Text, Text, NullWritable>{
-		private PriorityQueue<Movie> queue;
-		private Comparator<Movie> comp = new MovieComparator();
-		private int topK;
-		
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			String title = itr.nextToken().trim();
-			double avgRating = Double.parseDouble(itr.nextToken().trim());
-			insertQueue(queue, title, avgRating, topK);
-		}
-		protected void setup(Context context) throws IOException, InterruptedException{
-			Configuration conf = context.getConfiguration();
-			topK = conf.getInt("topK" , -1);
-			queue = new PriorityQueue<Movie> (topK, comp); //comp 기준으로 size가 topK인 queue 생성
-		}
-		
-		protected void cleanup(Context context) throws IOException, InterruptedException{
-			while(queue.size() != 0) { //queue가 0될 때까지
-				Movie movie = (Movie) queue.remove(); //head를 빼서
-				context.write(new Text(movie.toString()), NullWritable.get()); //emit
-			}
-		}
-		
-	}
-	
-	public static class TopKReducer extends Reducer<Text,NullWritable,Text,DoubleWritable>{
-		private PriorityQueue<Movie> queue;
-		private Comparator<Movie> comp = new MovieComparator();
-		private int topK;
-		
-		public void reduce(Text key, Iterable<NullWritable> value, Context context) throws IOException, InterruptedException{
-			StringTokenizer itr = new StringTokenizer(key.toString(), ","); // value가 아니라 key를 토큰화
-			
-			String title = itr.nextToken().trim();
-			double avgRating = Double.parseDouble(itr.nextToken().trim());
-			insertQueue(queue, title, avgRating, topK);
-		}
-		
-		protected void setup(Context context) throws IOException, InterruptedException{
-			Configuration conf = context.getConfiguration();
-			topK = conf.getInt("topK" , -1);
-			queue = new PriorityQueue<Movie> (topK, comp); //comp 기준으로 size가 topK인 queue 생성
-		}
-		
-		protected void cleanup(Context context) throws IOException, InterruptedException{
-			while(queue.size() != 0) { //queue가 0될 때까지
-				Movie movie = (Movie) queue.remove();
-				context.write(new Text(movie.getTitle()), new DoubleWritable(movie.getAvgRating())); 
-			}
-		}
-	}
 	public static void main(String[] args) throws Exception{
 		Configuration conf = new Configuration();
 		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 		
 		if(otherArgs.length != 3){
 			System.err.println("Usage: TopKAvg <in> <out>"); 
-   			System.exit(1);
+   			System.exit(2);
 		}
 		
 		conf.setInt("topK", Integer.parseInt(otherArgs[2]));
 		
-		String first_phase_result = "/first_phase_result";
+		Job job = new Job(conf, "IMDBStudent20191013");
+		job.setJarByClass(IMDBStudent20191013.class);
+		job.setMapperClass(TopKMovieMapper.class);
+		job.setReducerClass(TopKMovieReducer.class);
 		
-		Job job1 = new Job(conf, "AvgOfRating");
-		job1.setJarByClass(IMDBStudent20191013.class);
-		job1.setMapperClass(JoinNAvgMapper.class);
-		job1.setReducerClass(JoinNAvgReducer.class);
+		job.setMapOutputKeyClass(DoubleKey.class);
+		job.setMapOutputValueClass(Text.class);
 		
-		job1.setMapOutputKeyClass(DoubleKey.class);
-		job1.setMapOutputValueClass(Text.class);
+		job.setPartitionerClass(FirstPartitioner.class);
+		job.setGroupingComparatorClass(FirstGroupingComparator.class);
+		job.setSortComparatorClass(CompositeKeyComparator.class);
 		
-		job1.setPartitionerClass(FirstPartitioner.class);
-		job1.setGroupingComparatorClass(FirstGroupingComparator.class);
-		job1.setSortComparatorClass(CompositeKeyComparator.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		
-		job1.setOutputKeyClass(Text.class);
-		job1.setOutputValueClass(DoubleWritable.class);
-		
-		FileInputFormat.addInputPath(job1, new Path(otherArgs[0]));
-		FileOutputFormat.setOutputPath(job1, new Path(first_phase_result));
-		FileSystem.get(job1.getConfiguration()).delete(new Path(first_phase_result), true);
-		System.exit(job1.waitForCompletion(true) ? 0 : 1);
-		
-		Job job2 = new Job(conf, "TopKAvgOfRating");
-		job2.setJarByClass(IMDBStudent20191013.class);
-		job2.setMapperClass(TopKMapper.class);
-		job2.setReducerClass(TopKReducer.class);
-		job2.setOutputKeyClass(Text.class);
-		job2.setOutputValueClass(DoubleWritable.class);
-		FileInputFormat.addInputPath(job2, new Path(first_phase_result));
-		FileOutputFormat.setOutputPath(job2, new Path(otherArgs[1]));
-		FileSystem.get(job2.getConfiguration()).delete(new Path(otherArgs[1]), true);
-		System.exit(job2.waitForCompletion(true) ? 0 : 1);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		FileSystem.get(job.getConfiguration()).delete(new Path(otherArgs[1]), true);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	
 	}
 }
